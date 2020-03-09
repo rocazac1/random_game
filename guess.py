9595bed252c6062e7dcbb93135860c09ad61f41e
Robert Cazacu <roca@nuuday.dk>
2020-03-09 10:23:46 +0100

    added print out user choice meesage

--- guess.py
+++ guess.py
@@ -9,7 +9,7 @@ guesses = 0                    # Set guess counter to 0
 # while loop which loops betwen the lines until the number is guessed
 #print(number)
 while not guessed:
-    ans = input("What numer am I thinking of between 0 - 100? ")       # Ask for user input
+    ans = input("What number am I thinking of between 0 - 100? ")       # Ask for user input
     guesses += 1                                                       # Add +1 to  guess when loop runs
     clear_output()
     try:                                          
@@ -19,13 +19,13 @@ while not guessed:
             break
         
         elif int(ans) > number:
-            print("The number is a bit too high")
+            print("The {} number is a bit too high" .format(ans))
         
-        elif ans > 100:
-            print("Number is out of range")
+        elif int(ans) >= 100:
+            print("The {} number is out of range" .format(ans))
         
         else:
-            print("The number is too small")
+            print("The {} number is too small" .format(ans))
     
-    except ValueError:                                              # Catches the wrong type of data 
-        print("Please insert a number")
+    except ValueError:                                                           # Catches the wrong type of data 
+        print("{} is not a number. Please insert a number" .format(ans))
